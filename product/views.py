b996fa7422172cc24cae7d0b66561b5b558d29a1
AbdelilahChahbouni <abdelchahbouni@live.fr>
2023-08-29 21:05:38 +0100

    add related products in view and template

--- product/views.py
+++ product/views.py
@@ -13,6 +13,7 @@ class ProductDetails(DetailView):
 	def get_context_data(self , **kwargs):
 		context= super().get_context_data(**kwargs)
 		context['reviews']= Review.objects.filter(product=self.get_object())
+		context['related_products']= Product.objects.filter(brand=self.get_object().brand)
 		return context 
 
 
