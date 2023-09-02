42be9935c8e0cae895ae24b3101087ce99bf4686
AbdelilahChahbouni <abdelchahbouni@live.fr>
2023-09-02 15:47:54 +0100

    add tool bar debug

--- product/urls.py
+++ product/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path , include
 from .views import ProductList , ProductDetails , BrandList , BrandDetail
 
 
@@ -6,6 +6,7 @@ urlpatterns = [
 	path('' ,ProductList.as_view() ),
 	path('<slug:slug>',ProductDetails.as_view()),
 	path('brands/', BrandList.as_view()),
-	path('brands/<slug:slug>', BrandDetail.as_view())
+	path('brands/<slug:slug>', BrandDetail.as_view()),
+	path("__debug__/", include("debug_toolbar.urls")),
 
 ]
