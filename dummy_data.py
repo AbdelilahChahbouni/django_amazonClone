1e97dd3776b2bf88a619ffc79ce4dd95f0bd959f
AbdelilahChahbouni <abdelchahbouni@live.fr>
2023-09-02 11:50:01 +0100

    add dymmu data to Products database

--- dummy_data.py
+++ dummy_data.py
@@ -22,16 +22,37 @@ def seed_brand(number):
 			image = f'brands/{images[random.randint(0,13)]}'
 			)
 
-	print(f'seed {number} products successfully ')
+	print(f'seed {number} brands successfully ')
 
 
 
-def seed_product():
-	pass 
+def seed_product(number):
+	fake = Faker()
+	images = os.listdir(os.chdir("media/products"))
+	flags = ['New' , 'Features', 'Sales']
+
+	for _ in range(number):
+		Product.objects.create(
+			name = fake.name(),
+			image =f'products/{images[random.randint(0,13)]}',
+			flag = flags[random.randint(0,2)],
+			price = round(random.uniform(20.99 , 99.99),2),
+			sku = random.randint(1000,10000),
+			subtitle = fake.text(max_nb_chars=500),
+			description = fake.text(max_nb_chars=1000),
+			quantity = random.randint(0 , 50),
+			brand = Brand.objects.get(id = random.randint(1 , 200))
+			)
+
+
+	print(f'seed {number} Products successfully')
+
+
+#seed_brand(500)
+
+seed_product(1500)
 
 
 
 
-
-seed_brand(10)
 
