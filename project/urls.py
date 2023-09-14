ce5e94ce401cd0d2f9a852e7af880002a0bdce82
AbdelilahChahbouni <abdelchahbouni@live.fr>
2023-09-14 22:41:20 +0100

    add home page template

--- project/urls.py
+++ project/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/' , include('product.urls'))
+    path('' , include('settings.urls')),
+    path('products/' , include('product.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- settings/views.py
+++ settings/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.db.models import Count 
+
+from product.models import Product , Brand , Review
 
 # Create your views here.
+
+
+def home(request):
+	brands = Brand.objects.all().annotate(product_count = Count("product_brand"))
+	sale_product = Product.objects.filter(flag='Sales')[0:10]
+	new_product = Product.objects.filter(flag='New')[0:10]
+	feature_product = Product.objects.filter(flag='Feature')[:10]
+	reviews = Review.objects.all()
+	return render(request , "settings/home.html" , {
+		"brands":brands,
+		"sale_product":sale_product,
+		"new_product":new_product,
+		"feature_product":feature_product,
+		"reviews":reviews
+		})
+
+	
