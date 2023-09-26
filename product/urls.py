29abc53ffc4352c63848e4ae1d26e8d4dfff323f
AbdelilahChahbouni <abdelchahbouni@live.fr>
2023-09-26 22:11:19 +0100

    add jwt token

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path , include
 from .views import ProductList , ProductDetails , BrandList , BrandDetail , query_set
 from .api import ProductListAPI , ProductDetailAPI , BrandListAPI , BrandDetailAPI 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 urlpatterns = [
@@ -17,7 +21,12 @@ urlpatterns = [
 	path("api/list" , ProductListAPI.as_view()),
 	path("api/list/<int:pk>" , ProductDetailAPI.as_view()),
 	path("brands/api/list" , BrandListAPI.as_view()),
-	path("brands/api/list/<int:pk>" , BrandDetailAPI.as_view())
+	path("brands/api/list/<int:pk>" , BrandDetailAPI.as_view()),
+
+	# api token
+
+	path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 	
 
--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'product',
     'settings'
     
@@ -136,7 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
      'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 30
