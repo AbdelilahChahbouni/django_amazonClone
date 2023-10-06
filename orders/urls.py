996409ebff62522ea2ced46b29b80f1ac1d0ac78
AbdelilahChahbouni <abdelchahbouni@live.fr>
2023-10-06 20:50:21 +0100

    add remove cart

--- orders/urls.py
+++ orders/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import OrderList , checkout , add_to_cart
+from .views import OrderList , checkout , add_to_cart , remove_from_cart
 
 app_name='orders'
 
 urlpatterns = [
 	path('', OrderList.as_view()),
 	path('checkout/' , checkout),
-	path('add_to_cart' , add_to_cart, name='add_to_cart')
+	path('add_to_cart' , add_to_cart, name='add_to_cart'),
+	path('<int:id>/remove_from_cart' , remove_from_cart)
+
 
 
 
