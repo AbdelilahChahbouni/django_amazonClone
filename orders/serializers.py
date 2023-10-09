ae644f48c4a2cb8e019958795abbf2bbd7180346
AbdelilahChahbouni <abdelchahbouni@live.fr>
2023-10-09 22:10:10 +0100

    add cart , cart detail serializer

---
+++ orders/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework.serializers import serializers
+from .models import Cart , CartDetail , Order , OrderDetail
+
+
+
+
+class CartDetailSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = CartDetail
+		fields = "__all__"
+
+
+
+class CartSerializer(serializers.ModelSerializer):
+	cart_detail = CartDetailSerializer(many=True)
+	class Meta:
+		model = Cart
+		fields = "__all__"
+
+
