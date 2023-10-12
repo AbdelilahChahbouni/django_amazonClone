49db211a851f2620b05b7d5efa21e4b15f6fa0ea
AbdelilahChahbouni <abdelchahbouni@live.fr>
2023-10-12 20:57:25 +0100

    add delete and post api function

--- orders/api.py
+++ orders/api.py
@@ -20,11 +20,36 @@ class CartDetailCreateAPI(generics.GenericAPIView):
 		return Response({"cart":data})
 
 
-	def post(self , requset , *args , **kwargs):
-		pass 
+	def post(self , request , *args , **kwargs):
+		user = User.objects.get(username=self.kwargs["username"])
+		product = Product.objects.get(id = request.data["product_id"])
+		new_qty = int(request.data['quantity'])
+		cart = Cart.objects.get(user=user , status_cart="InProgress")
+		cart_detail,created = CartDetail.objects.get_or_create(cart=cart,product=product)
+		cart_detail.quantity = new_qty
+		cart_detail.total_price = round((new_qty * product.price),2)
+		cart_detail.save()
+
+		cart = Cart.objects.get(user=user , status_cart="InProgress")
+		data = CartSerializer(cart).data
+		return Response({"message":"product added succssfully", "cart":data})
 
 
-	def delete(self , requset , *args , **kwargs):
-		pass 
+
+
+
+
+
+	def delete(self , request , *args , **kwargs):
+		user = User.objects.get(username=self.kwargs["username"])
+		cart_detail = CartDetail.objects.get(id=request.data["cart_detail_id"])
+		cart_detail.quantity = 2
+		cart_detail.delete()
+		cart = Cart.objects.get(user=user , status_cart="InProgress")
+		data = CartSerializer(cart).data
+		return Response({"message":"cart detail deleted succssfully", "cart":data})
+
+
+
 
 
--- orders/models.py
+++ orders/models.py
@@ -9,27 +9,28 @@ from utils import generate_code
 CART_STAUS = (('InProgress' , 'InProgress' ),( 'Completed' , 'Completed' ))
 
 class Cart(models.Model):
- user = models.ForeignKey(User,related_name="user_cart",on_delete=models.SET_NULL,null=True,blank=True)
- status_cart = models.CharField(max_length=10,choices=CART_STAUS)
+    user = models.ForeignKey(User,related_name="user_cart",on_delete=models.SET_NULL,null=True,blank=True)
+    status_cart = models.CharField(max_length=10,choices=CART_STAUS)
 
- def __str__(self):
-  return str(self.user)
+    def __str__(self):
+        return str(self.user)
 
- def cart_total(self):
-  total = 0
-  for item in self.cart_detail.all():
-  	total += item.total_price
-  return round(total,2)
+    def cart_total(self):
+        total = 0
+        for item in self.cart_detail.all():
+            total += item.total_price
+        
 
 
 class CartDetail(models.Model):
- cart = models.ForeignKey(Cart , related_name='cart_detail' , on_delete=models.CASCADE)
- product = models.ForeignKey(Product , related_name="cart_product" , on_delete=models.SET_NULL,null=True,blank=True)
- quantity = models.IntegerField(default=1)
- total_price = models.FloatField(null=True,blank=True)
 
- def __str__(self):
-  return str(self.cart)
+    cart = models.ForeignKey(Cart , related_name='cart_detail' , on_delete=models.CASCADE)
+    product = models.ForeignKey(Product , related_name="cart_product" , on_delete=models.SET_NULL,null=True,blank=True)
+    quantity = models.IntegerField(default=1)
+    total_price = models.FloatField(null=True,blank=True)
+
+    def __str__(self):
+        return str(self.cart)
 
 
 
@@ -39,40 +40,42 @@ class CartDetail(models.Model):
 ORDER_STAUS = (("Recieved" , "Recieved"),("Processed" , "Processed"),("Shipped" , "Shipped"),("Delivered" , "Delivered"))
 
 class Order(models.Model):
- user = models.ForeignKey(User,related_name="user_order",on_delete=models.SET_NULL,null=True,blank=True)
- status_order = models.CharField(max_length=10,choices=ORDER_STAUS) 
- code = models.CharField(default=generate_code.generate_code,max_length=40)
- order_time = models.DateTimeField(default=timezone.now)
- delivery_time = models.DateTimeField(null=True, blank=True) 
- coupon = models.ForeignKey('Coupon' , related_name='order_coupon' , on_delete=models.SET_NULL , null=True,blank=True)
- total_after_coupon = models.FloatField(null=True,blank=True)
 
- def __str__(self):
-  return str(self.user)
+    user = models.ForeignKey(User,related_name="user_order",on_delete=models.SET_NULL,null=True,blank=True)
+    status_order = models.CharField(max_length=10,choices=ORDER_STAUS) 
+    code = models.CharField(default=generate_code.generate_code,max_length=40)
+    order_time = models.DateTimeField(default=timezone.now)
+    delivery_time = models.DateTimeField(null=True, blank=True) 
+    coupon = models.ForeignKey('Coupon' , related_name='order_coupon' , on_delete=models.SET_NULL , null=True,blank=True)
+    total_after_coupon = models.FloatField(null=True,blank=True)
+
+    def __str__(self):
+        return str(self.user)
 
 
 class OrderDetail(models.Model):
- cart = models.ForeignKey(Order , related_name='order_detail' , on_delete=models.CASCADE)
- product = models.ForeignKey(Product , related_name="order_product" , on_delete=models.SET_NULL,null=True,blank=True)
- price = models.FloatField()
- quantity = models.IntegerField()
- total_price = models.FloatField(null=True,blank=True)
+    cart = models.ForeignKey(Order , related_name='order_detail' , on_delete=models.CASCADE)
+    product = models.ForeignKey(Product , related_name="order_product" , on_delete=models.SET_NULL,null=True,blank=True)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    total_price = models.FloatField(null=True,blank=True)
 
- def __str__(self):
-  return str(self.cart)
+    def __str__(self):
+        return str(self.cart)
 
 
 class Coupon(models.Model):
- code = models.CharField(max_length=20)
- discount = models.IntegerField()
- quantity = models.IntegerField()
- start_date = models.DateTimeField(default=timezone.now)
- end_time = models.DateTimeField(null=True,blank=True)
+    code = models.CharField(max_length=20)
+    discount = models.IntegerField()
+    quantity = models.IntegerField()
+    start_date = models.DateTimeField(default=timezone.now)
+    end_time = models.DateTimeField(null=True,blank=True)
 
- def __str__(self):
-  return self.code 
+    def __str__(self):
+        return self.code 
 
- def save(self, *args , **kwargs):
-  week = datetime.timedelta(days=7)
-  self.end_time = self.start_date + week
-  super(coupon, self).save(*args , **kwargs)
+    def save(self, *args , **kwargs):
+
+        week = datetime.timedelta(days=7)
+        self.end_time = self.start_date + week
+        super(coupon, self).save(*args , **kwargs)
