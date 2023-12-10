13beb69c2cb598c5959339dbe5c09f3bed74c12e
AbdelilahChahbouni <abdelchahbouni@live.fr>
2023-12-10 13:29:54 +0100

    add signup and activate

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import signup
+from .views import signup , activate , profile
 
 app_name = "accounts"
 
 urlpatterns = [
     path("signup" ,signup ,name="singup"),
+    path('<str:username>/activate', activate , name='activate')
 ]
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,54 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from django.core.mail import send_mail
+from .forms import SignupForm , ActivationForm
+from django.contrib.auth.models import User
+from .models import Profile 
 
 
 
 def signup(request):
-    pass
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            user=form.save(commit=False)
+            user.is_active = False
+            user.save()
 
-def validation(request):
-    pass
+            profile = Profile.objects.get(user__username = username)
+            send_mail(
+            "Code Activation",
+            f"Welcom {username}\n use this code {profile.code} to activate your acoount",
+            "abdelchahbouni@live.fr",
+            [email],
+            fail_silently=False,
+        )
+            return redirect(f"/accounts/{username}/activate")
+
+    else:
+        form = SignupForm()
+    return render(request , "registration/register.html" , {"form":form})
+
+
+
+def activate(request, username):
+    profile = Profile.objects.get(user__username = username)
+    if request.method == "POST":
+        form = ActivationForm(request.POST)
+        if form.is_valid():
+            code = form.cleaned_data['code']
+            if code == profile.code:
+                profile.code = ''
+                profile.save()
+                user = User.objects.get(username=profile.user)
+                user.is_active = True
+                user.save()
+
+                return redirect('/accounts/login')
+    else:
+        form = ActivationForm()
+    return render(request, "registration/activate.html" , {'form':form})
 
 def profile(request):
     pass
